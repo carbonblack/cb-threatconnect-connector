d24ca24627e1ba39222a288a1060093ffa9e791b
Zachary Estep <zestep@carbonblack.com>
2019-08-29 16:00:14 -0400

    Updates to test infrastructure

--- tests/drivertest.py
+++ tests/drivertest.py
@@ -1,6 +1,36 @@
 import unittest
+import threading
 import traceback 
-import threatconnect
+from cbopensource.driver import threatconnect
+from tests.utils.threatconnect_mock_server import get_mocked_server
+
+class TestTcDriverMockedServer(unittest.TestCase):
+
+
+    #def run_flask(): 
+     #    self.mock_tc_server.run('127.0.0.1', 7982, debug=False)
+
+    def setUp(self):
+        self.mock_tc_server = get_mocked_server("tests/data")
+        kwargs = { "sources":"Carbon Black",
+         "url":"http://localhost:7982/api",
+          "web_url":"http://localhost:7982/auth",
+          "api_key":"adfasfdsa",
+          "secret_key":"asfdsafdsa",
+          "default_org":"Carbon Black",
+          "ioc_types":"file"}
+        tcconfig = threatconnect.ThreatConnectConfig(**kwargs)
+        threatconnect.ThreatConnectDriver.initialize(tcconfig)
+        self.driver = threatconnect.ThreatConnectDriver(tcconfig)
+        t = threading.Thread(target=self.mock_tc_server.run, args=('127.0.0.1', 7982 ), kwargs={ "debug" : False })
+        t.daemon = True
+        t.start()   
+
+    def test_driver_against_mock(self):
+        reports = threatconnect.ThreatConnectDriver.generate_reports(self.driver)
+        self.assertIsNotNone(reports, "GOT REPORTS FROM DRIVER")
+
+
 
 class TestTcConfig(unittest.TestCase):
     def test_tc_config(self):
