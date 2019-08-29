aff669c28a6cf5e8877b82a3f8052c9c576dc993
Zachary Estep <zestep@carbonblack.com>
2019-08-29 14:07:28 -0400

    More config tests

--- cbopensource/driver/drivertest.py
+++ cbopensource/driver/drivertest.py
@@ -1,6 +1,6 @@
 import unittest
 import traceback 
-import theatconnect
+import threatconnect
 
 class TestTcConfig(unittest.TestCase):
     def test_tc_config(self):
@@ -9,11 +9,35 @@ class TestTcConfig(unittest.TestCase):
           "web_url":"https://api.sandbox.threatconnect.com/auth",
           "api_key":"adfasfdsa",
           "secret_key":"asfdsafdsa",
-          "default_org":"Carbon Black"}
+          "default_org":"Carbon Black",
+          "ioc_types":"file"}
         try:
-            tcconfig = theatconnect.ThreatConnectConfig(**kwargs)
+            tcconfig = threatconnect.ThreatConnectConfig(**kwargs)
         except Exception as e:
             self.fail("Unexpected config error when construction threat connect config {0}".format(traceback.format_exc()))
+
+    def test_tc_config_all_types_if_none(self):
+        kwargs = { "sources":"Carbon Black",
+         "url":"https://api.sandbox.threatconnect.com/api",
+          "web_url":"https://api.sandbox.threatconnect.com/auth",
+          "api_key":"adfasfdsa",
+          "secret_key":"asfdsafdsa",
+          "default_org":"Carbon Black"}
+        try:
+            tcconfig = threatconnect.ThreatConnectConfig(**kwargs)
+        except Exception as e:
+            self.fail("Unexpected config error when construction threat connect config {0}".format(traceback.format_exc()))
+
+    def test_tc_config_bad_ioc_min(self):
+        kwargs = { "sources":"Carbon Black",
+         "url":"https://api.sandbox.threatconnect.com/api",
+          "web_url":"https://api.sandbox.threatconnect.com/auth",
+          "api_key":"adfasfdsa",
+          "secret_key":"asfdsafdsa",
+          "default_org":"Carbon Black",
+          "ioc_min_rating": 6}
+        with self.assertRaises(ValueError):
+            tcconfig = threatconnect.ThreatConnectConfig(**kwargs)
 
 if __name__ == "__main__":
     unittest.main()
