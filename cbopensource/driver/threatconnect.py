c6171b6c9b50e4e55cb28e0e74dc2411991c1faa
John Capolino <jcapolino@carbonblack.com>
2019-09-04 13:50:49 -0500

    Added comments and fixed null id error.

--- cbopensource/driver/threatconnect.py
+++ cbopensource/driver/threatconnect.py
@@ -64,6 +64,12 @@ class IocType(Enum):
 
 
 class _TcIndicator(object):
+    """This class wraps an indicator dict that comes from the tcex lib.
+
+    This class pulls data out of the indicator dict that comes from tcex and allows for access to it's data
+    in a cleaner form.
+    """
+
     def __init__(self, indicator, source, ioc_type, key, value):
         self._indicator = indicator
         self._source = source
@@ -78,10 +84,18 @@ class _TcIndicator(object):
 
     @property
     def score(self):
+        """Returns the CB IOC report score (from 0 to 100) which is calculated from the threatconnect rating.
+
+        :return: The CB IOC report score.
+        """
         return int(self.rating * 20)
 
     @property
     def rating(self):
+        """Returns the threatconnect rating (on a scale from 0 (undefined) to 5)
+
+        :return: The threatconnect rating.
+        """
         return int(max(0, min(5, self._indicator.get('rating', 0))))
 
     @property
@@ -102,6 +116,10 @@ class _TcIndicator(object):
 
     @property
     def timestamp(self):
+        """Converts the text date and time to a EPOC integer in GMT.
+
+        :return: An integer representing the EPOC date and time in GMT.
+        """
         if not self._datetime:
             date = self._indicator.get('lastModified', None) or self._indicator['dateAdded']
             dt = datetime.strptime(date, "%Y-%m-%dT%H:%M:%SZ")
@@ -122,6 +140,11 @@ class _TcIndicator(object):
 
 
 class IocFactory(object):
+    """The base class for all Ioc Factories.
+
+    An IOC Factory is capable of creating a CB compatible IOC from a threatconnect indicator.
+    Part of the creation process is validation of the indicator data as well as filtering based on config settings.
+    """
     _ioc_map = {}
 
     def __str__(self):
@@ -129,10 +152,21 @@ class IocFactory(object):
 
     @classmethod
     def from_text(cls, text):
+        """Converts text into an IocFactory.
+
+        :param text: The IOC type in text form.
+        :return: The IocFactory that handles that IOC type.
+        """
         return cls._ioc_map[IocType(text.strip().upper())]
 
     @classmethod
     def from_text_to_list(cls, text, all_if_none):
+        """Converts a comma separated list into a list of IocFactories.
+
+        :param text: A comma separated list.
+        :param all_if_none: If text is empty or None, and all_if_none is set, returns a list of all IOC factories.
+        :return: A list of Ioc Factories
+        """
         if text:
             return [cls.from_text(t) for t in text.split(",")]
         elif all_if_none:
@@ -145,6 +179,12 @@ class IocFactory(object):
 
     @classmethod
     def filter_ioc(cls, indicator, filters):
+        """Filters out an IOC if it exists in the filters list.
+
+        :param indicator: The indicator to check against the filters
+        :param filters: A set of values to check the indicator against to see if it needs to be filtered out.
+        :return: The indicator if not filtered out.  None if the indicator is filtered out.
+        """
         if indicator.value is None:
             return None
         if filters:
@@ -155,9 +195,19 @@ class IocFactory(object):
 
     @classmethod
     def get_indicator_value(cls, indicator, keys):
+        """This is a helper that grabs the value from the indicator from the prioritized list of keys.
+
+        :param indicator: The indicator to extract the value.
+        :param keys: The list of valid keys to attempt the value extraction.
+        :return: The value (or None if no key was present in the indicator) and the corresponding key the value
+        from which the value was taken.
+        """
         value, key = next(((indicator.get(key), key) for key in keys if key in indicator), (None, None))
         if value is None:
-            _logger.debug("Expected key(s) {0} missing from indicator of type {1}.".format(keys, cls._name))
+            # The following log message can spam the log file if debug logging is turned on.
+            # We are no longer going to log this message and so now it will skip this indicator quietly.
+            # _logger.debug("Expected key(s) {0} missing from indicator of type {1}.".format(keys, cls._name))
+            pass
         return value, key
 
     def __repr__(self):
@@ -207,12 +257,19 @@ class IocGrouping(Enum):
 
     @classmethod
     def from_text(cls, text, default):
+        """ Converts text into an IocGrouping
+
+        :param text: The text used to determine which IocGrouping to return.
+        :param default: If text is empty or None, returns this value.
+        :return: The IocGrouping that matches the text.
+        """
         if text:
             return cls(text.strip().upper())
         return default
 
 
 class _Sources(object):
+    """Contains a list of sources specified by either a * (meaning all sources) or a comma separated list."""
     def __init__(self, sources="*"):
         sources = sources.strip()
         self._all = sources == "*"
@@ -233,12 +290,18 @@ class _Sources(object):
         return "Sources({0})".format(self.__str__())
 
     def __contains__(self, key):
+        """A convenience function to support the python "in" syntax.
+
+        :param key: The key to check if in this list of sources.
+        :return: True if key is in the list of sources otherwise False.  Always returns true if sources is *
+        """
         if self.all:
             return True
         return str(key) in self._values
 
 
 class ThreatConnectConfig(object):
+    """This class is used to configure the ThreatConnect Driver to pull data from threatconnect."""
     def __init__(self,
                  sources="*",
                  url=None,
@@ -290,9 +353,11 @@ class ThreatConnectConfig(object):
     
     @staticmethod
     def _log_entry(title, value, padding=20):
+        """A helper function to quickly format and log a config entry."""
         _logger.info("{0:{2}}: {1}".format(title, value, padding))
     
     def _log_config(self):
+        """Writes the current configuration out to the log."""
         _logger.info("ThreatConnect Driver configuration loaded.")
         self._log_entry("Sources", self.sources)
         self._log_entry("Url", self.url)
@@ -312,6 +377,7 @@ class ThreatConnectConfig(object):
         self._log_entry("Max Reports", self.max_reports or "Disabled")
 
     def _read_filter_file(self, filter_file):
+        """Reads in the data from one of the filter files if the file exists."""
         if not filter_file:
             return set()
         try:
@@ -322,6 +388,7 @@ class ThreatConnectConfig(object):
 
 
 class _TcSource(object):
+    """This class wraps a threatconnect source so that it can be used more easily."""
     def __init__(self, raw_source):
         self._source = raw_source
         self._id = int(raw_source["id"])
@@ -345,14 +412,28 @@ class _TcSource(object):
         return str(other) == str(self)
 
     def generate_id(self, score, ioc_type=None):
-        # Moving the id over 12 bits and ioc_type 8 bits to make room for a decimal up
-        # to 256 though we only need it up to 100.
+        """Creates a unique and repeatable ID based on this source, the score, and ioc type.
+
+        It's very important that the id is repeatable meaning the id remains the same after each pull from
+        threatconnect.  The id generated here is used to create the report which CB uses to update it's feed reports.
+        It's also important that the id is unique so that it doesn't conflict with other reports.
+
+        :param score: The score of the report/indicator
+        :param ioc_type: The type of ioc.  This can be None which would mean all types.
+        :return: The generated id.
+        """
+        # Moving the id over 12 bits and ioc_type 8 bits.
+        # That means there can be 16 ioc_types and a score up to 256 though we only need it up to 100.
         generated_id = (self._id << 12) | (IocType.get_index(ioc_type) << 8) | score
         _logger.debug("Generating id for source [{0}] with a score of {1}: {2}".format(self._name, score, generated_id))
         return generated_id
 
 
 def _TcSources(client):
+    """Generates wrapped sources pulled from threatconnect.
+
+    :param client: The threatconnect client.
+    """
     try:
         owners = [_TcSource(o) for o in client().ti.owner().many()]
         _logger.debug("Sources retrieved from threatconnect: {0}".format(owners))
@@ -371,6 +452,11 @@ def _TcSources(client):
 
 
 class _TcReportGenerator(object):
+    """The base class for all report generators.
+
+    ReportGenerators do the work of communicating with the threatconnect client, pulling data, validating the data,
+    converting the data into valid IOCs and producing the appropriate reports.
+    """
     _parameters = {'includes': ['additional', 'attributes', 'labels', 'tags']}
 
     def __init__(self, client):
@@ -378,9 +464,13 @@ class _TcReportGenerator(object):
         self._notified_max_reports = False
 
     def generate_reports(self):
+        """Creates a list of reports with data pulled from the threatconnect client.
+
+        :return: The list of reports.
+        """
         count = 0
         for source in _TcSources(self._client):
-            _logger.debug("Pulling IOCs from source: [{0}]".format(source))
+            _logger.info("Pulling IOCs from source: [{0}]".format(source))
             source_count = 0
             for ioc_type in self._client.config.ioc_types:
                 try:
@@ -389,6 +479,7 @@ class _TcReportGenerator(object):
                         ioc = ioc_type.create(indicator, source, self._client.config)
                         if ioc:
                             if not self._add_to_report(ioc):
+                                # We are no longer able to continue so we drop out gracefully.
                                 if not source_count:
                                     _logger.info("No IOCs imported for source: [{0}]".format(str(source)))
                                 return count, len(self.reports), self.reports
@@ -396,18 +487,25 @@ class _TcReportGenerator(object):
                             source_count += 1
 
                 except Exception as e:
+                    # This is a blanket exception handler because we don't want any exception to stop the entire
+                    # pull from occurring.
                     _logger.exception("Failed to read IOCs for source {0} and IOC type {1}".format(source, ioc_type))
             if not source_count:
                 _logger.info("No IOCs found for source: [{0}]".format(source))
         return count, len(self.reports), self.reports
 
     def max_reports_notify(self):
+        """Reports that the maximum number of reports has been reached.
+
+        This is a convenience function that makes sure the max reached log is only written once.
+        """
         if not self._notified_max_reports:
             self._notified_max_reports = True
             _logger.warning("The maximum number of reports ({0}) has been reached.".format(
                 self._client.config.max_reports))
 
     def _filters(self):
+        """Adds any filters that may be necessary."""
         filters = self._client().ti.filters()
         if self._client.config.ioc_min_rating:
             filters.add_filter("rating", ">", str(self._client.config.ioc_min_rating - 1))
@@ -415,6 +513,7 @@ class _TcReportGenerator(object):
 
 
 class _ExpandedReportGenerator(_TcReportGenerator):
+    """This report generator creates reports that contain only one IOC per report."""
     def __init__(self, client):
         _TcReportGenerator.__init__(self, client)
         self._reports = []
@@ -442,6 +541,10 @@ class _ExpandedReportGenerator(_TcReportGenerator):
 
 
 class _BaseCondensedReportGenerator(_TcReportGenerator):
+    """This report generator is the base class for condensed report generators.
+
+    Condensed report generators package multiple IOCs into a single report.
+    In a lot of cases this can be more efficient but this has its own set of consequences."""
     def __init__(self, client):
         _TcReportGenerator.__init__(self, client)
         self._reports = []
@@ -460,6 +563,11 @@ class _BaseCondensedReportGenerator(_TcReportGenerator):
         raise NotImplementedError()
 
     def _get_report(self, indicator):
+        """ Finds an existing report to place the indicator or creates one if none exist.
+
+        :param indicator: The indicator for which to find/create the report.
+        :return: The report or None if one cannot be created (hit the max report count limit).
+        """
         score_list = self._get_score_list(indicator)
         report = score_list[indicator.score]
         if not report:
@@ -494,6 +602,9 @@ class _BaseCondensedReportGenerator(_TcReportGenerator):
 
     @property
     def reports(self):
+        """Converts the IOC sets into lists for json encoding compatibility.
+        This should be done at the end of the run as lists are not nearly as efficient as sets.
+        """
         if not self._converted_sets:
             for report in self._reports:
                 for k, v in report["iocs"].iteritems():
@@ -503,6 +614,12 @@ class _BaseCondensedReportGenerator(_TcReportGenerator):
 
 
 class _MaxCondensedReportGenerator(_BaseCondensedReportGenerator):
+    """Generates reports based on creating as few reports as possible.
+
+    The Max condensed report generator creates a single report that contains all IOCs
+    for a particular source and score.  So each source with a particular score will have all associated IOCs
+    regardless of the ioc type.
+    """
     def __init__(self, client):
         _BaseCondensedReportGenerator.__init__(self, client)
         self._reports_map = {}
@@ -527,10 +644,17 @@ class _MaxCondensedReportGenerator(_BaseCondensedReportGenerator):
         return "{0} - {1}".format(indicator.source, indicator.score)
 
     def _generate_id(self, indicator):
-        indicator.source.generate_id(indicator.score)
+        return indicator.source.generate_id(indicator.score)
 
 
 class _CondensedReportGenerator(_BaseCondensedReportGenerator):
+    """Generates reports based on creating as few reports as possible but more than MaxCondensed.
+
+    The Condensed report generator creates a single report that contains all IOCs
+    for a particular source, score, and type.  So each source with a particular score and type of ioc will contain all
+    relevant IOCs.  It's similar to MaxCondensed except that the ioc type is also included to break the reports up
+    into smaller chunks.
+    """
     def __init__(self, client):
         _BaseCondensedReportGenerator.__init__(self, client)
         self._reports_map = {}
@@ -570,6 +694,7 @@ _reportGenerators = {
 
 
 class ThreatConnectClient(object):
+    """Wraps the tcex library to make it easier to use."""
     def __init__(self, config):
         self._config = config
         
@@ -593,12 +718,38 @@ class ThreatConnectClient(object):
 
 
 class ThreatConnectDriver(object):
+    """The main interface into extracting report data from threatconnect."""
     _client = None
 
     def __init__(self, config):
         self._config = config
     
     def generate_reports(self):
+        """Connects the the ThreatConnectClient and generates reports from data pulled from the client.
+
+        Format of the reports generated:
+        [{
+            "title": "REPORT TITLE",
+            "timestamp": 1547441523,
+            "iocs": {
+                "IOC_TYPE": [
+                    "IOC",
+                    "IOC",
+                    ...
+                ],
+                "IOC_TYPE": [ ...
+                ]
+            },
+            "score": 0 through 100,
+            "link": "https://...",
+            "id": NUMBER
+        },
+        { ...
+        }]
+
+
+        :return: A list of reports in dictionary format.
+        """
         _logger.info("Starting report retrieval.")
 
         if not self._client:
@@ -610,5 +761,10 @@ class ThreatConnectDriver(object):
         return reports
 
     @classmethod
-    def initialize(cls, config):
-        cls._client = ThreatConnectClient(config)
+    def initialize(cls, config, client=None):
+        """This method MUST be called on the main thread if the ThreatConnectClient is not being passed in.
+
+        :param config: The ThreatConnectConfig.
+        :param client: A ThreatConnectClient.  Normally this is not passed in and is created by this function.
+        """
+        cls._client = client or ThreatConnectClient(config)
--- tests/test_driver.py
+++ tests/test_driver.py
@@ -1,83 +1,86 @@
 import unittest
 import threading
-import traceback 
+import traceback
 from cbopensource.driver import threatconnect
 from tests.utils.threatconnect_mock_server import get_mocked_server
+
 
 class TestTcDriverMockedServer(unittest.TestCase):
 
     def setUp(self):
         self.mock_tc_server = get_mocked_server("tests/data")
-        kwargs = { "sources":"*",
-         "url":"http://localhost:7982/api",
-          "web_url":"http://localhost:7982/auth",
-          "api_key":"adfasfdsa",
-          "secret_key":"asfdsafdsa",
-          "default_org":"Carbon Black",
-          "ioc_types":"File,Address,Host"}
+        kwargs = {"sources": "*",
+                  "url": "http://localhost:7982/api",
+                  "web_url": "http://localhost:7982/auth",
+                  "api_key": "adfasfdsa",
+                  "secret_key": "asfdsafdsa",
+                  "default_org": "Carbon Black",
+                  "ioc_types": "File,Address,Host"}
         tcconfig = threatconnect.ThreatConnectConfig(**kwargs)
         threatconnect.ThreatConnectDriver.initialize(tcconfig)
         self.driver = threatconnect.ThreatConnectDriver(tcconfig)
-        t = threading.Thread(target=self.mock_tc_server.run, args=('127.0.0.1', 7982 ), kwargs={ "debug" : False })
+        t = threading.Thread(target=self.mock_tc_server.run, args=('127.0.0.1', 7982), kwargs={"debug": False})
         t.daemon = True
-        t.start()   
+        t.start()
 
     def test_driver_against_mock(self):
         reports = threatconnect.ThreatConnectDriver.generate_reports(self.driver)
-        print(reports)
-        #self.assertIsNotNone(reports, "GOT REPORTS FROM DRIVER")
-        self.assertGreater(len(reports),1,"Didn't get any reports!")
+        self.assertTrue(reports, "Didn't get any reports!")
+
 
 class TestTcConfig(unittest.TestCase):
     def test_tc_config(self):
-        kwargs = { "sources":"Carbon Black",
-         "url":"https://api.sandbox.threatconnect.com/api",
-          "web_url":"https://api.sandbox.threatconnect.com/auth",
-          "api_key":"adfasfdsa",
-          "secret_key":"asfdsafdsa",
-          "default_org":"Carbon Black",
-          "ioc_types":"file"}
-        try:
-            tcconfig = threatconnect.ThreatConnectConfig(**kwargs)
-        except Exception as e:
-            self.fail("Unexpected config error when construction threat connect config {0}".format(traceback.format_exc()))
+        kwargs = {"sources": "Carbon Black",
+                  "url": "https://api.sandbox.threatconnect.com/api",
+                  "web_url": "https://api.sandbox.threatconnect.com/auth",
+                  "api_key": "adfasfdsa",
+                  "secret_key": "asfdsafdsa",
+                  "default_org": "Carbon Black",
+                  "ioc_types": "file"}
+        tcconfig = threatconnect.ThreatConnectConfig(**kwargs)
 
     def test_tc_config_all_types_if_none(self):
-        kwargs = { "sources":"Carbon Black",
-         "url":"https://api.sandbox.threatconnect.com/api",
-          "web_url":"https://api.sandbox.threatconnect.com/auth",
-          "api_key":"adfasfdsa",
-          "secret_key":"asfdsafdsa",
-          "default_org":"Carbon Black"}
-        try:
-            tcconfig = threatconnect.ThreatConnectConfig(**kwargs)
-        except Exception as e:
-            self.fail("Unexpected config error when construction threat connect config {0}".format(traceback.format_exc()))
+        kwargs = {"sources": "Carbon Black",
+                  "url": "https://api.sandbox.threatconnect.com/api",
+                  "web_url": "https://api.sandbox.threatconnect.com/auth",
+                  "api_key": "adfasfdsa",
+                  "secret_key": "asfdsafdsa",
+                  "default_org": "Carbon Black"}
+        tcconfig = threatconnect.ThreatConnectConfig(**kwargs)
 
     def test_tc_config_bad_ioc_min(self):
-        kwargs = { "sources":"Carbon Black",
-         "url":"https://api.sandbox.threatconnect.com/api",
-          "web_url":"https://api.sandbox.threatconnect.com/auth",
-          "api_key":"adfasfdsa",
-          "secret_key":"asfdsafdsa",
-          "default_org":"Carbon Black",
-          "ioc_min_rating": 6}
+        kwargs = {"sources": "Carbon Black",
+                  "url": "https://api.sandbox.threatconnect.com/api",
+                  "web_url": "https://api.sandbox.threatconnect.com/auth",
+                  "api_key": "adfasfdsa",
+                  "secret_key": "asfdsafdsa",
+                  "default_org": "Carbon Black",
+                  "ioc_min_rating": 6}
+        with self.assertRaises(ValueError):
+            tcconfig = threatconnect.ThreatConnectConfig(**kwargs)
+
+        kwargs["ioc_min_rating"] = -1
+        with self.assertRaises(ValueError):
+            tcconfig = threatconnect.ThreatConnectConfig(**kwargs)
+
+        kwargs["ioc_min_rating"] = "text"
         with self.assertRaises(ValueError):
             tcconfig = threatconnect.ThreatConnectConfig(**kwargs)
 
     def test_tc_config_bad_configs(self):
-        kwargs = { "sources":"Carbon Black",
-         "url":"https://api.sandbox.threatconnect.com/api",
-          "web_url":"https://api.sandbox.threatconnect.com/auth",
-          "api_key":"adfasfdsa",
-          "secret_key":"asfdsafdsa",
-          "default_org":"Carbon Black"}
-        toremove = ["url","web_url","api_key","secret_key"]
+        kwargs = {"sources": "Carbon Black",
+                  "url": "https://api.sandbox.threatconnect.com/api",
+                  "web_url": "https://api.sandbox.threatconnect.com/auth",
+                  "api_key": "adfasfdsa",
+                  "secret_key": "asfdsafdsa",
+                  "default_org": "Carbon Black"}
+        toremove = ["url", "web_url", "api_key", "secret_key"]
         for remove in toremove:
             with self.assertRaises(ValueError):
                 mykwargs = kwargs.copy()
                 del mykwargs[remove]
                 tcconfig = threatconnect.ThreatConnectConfig(**mykwargs)
+
 
 if __name__ == "__main__":
     unittest.main()
